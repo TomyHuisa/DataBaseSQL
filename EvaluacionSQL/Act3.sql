d0f66b9864ce52afdfbd857e14d69afb37723948
Tomy Huisa <huisatomy2007@gmail.com>
2024-05-14 17:11:53 -0300

    Act8_Terminado

--- EvaluacionSQL/Act3.sql
+++ EvaluacionSQL/Act3.sql
@@ -1,6 +1,2 @@
 -- Act3 --
 
-SELECT  d.gender AS Personas, count(p.license_id) AS Licencias FROM drivers_license d
-LEFT JOIN person p ON p.license_id = d.id
-GROUP BY license_id
-ORDER BY Licencias DESC;
--- EvaluacionSQL/Act8.sql
+++ EvaluacionSQL/Act8.sql
@@ -1,2 +1,7 @@
 -- Act8 --
 
+SELECT d.car_make, d.car_model, c.annual_income FROM drivers_license d
+JOIN person p ON d.id = p.license_id
+JOIN income c ON p.ssn = c.ssn
+WHERE c.annual_income < 35000
+ORDER BY annual_income ASC;
