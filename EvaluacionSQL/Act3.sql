04b3538f512df7188b1a75a67061c25e70373319
Tomy Huisa <huisatomy2007@gmail.com>
2024-05-14 17:14:22 -0300

    Act3_Terminado

--- EvaluacionSQL/Act3.sql
+++ EvaluacionSQL/Act3.sql
@@ -1,6 +1,6 @@
 -- Act3 --
 
-SELECT  p.name AS Nombres, d.gender AS Personas, count(p.license_id) AS Licencias FROM drivers_license d
+SELECT  p.name AS Personas, d.gender AS Genero, count(p.license_id) AS Licencias FROM drivers_license d
 LEFT JOIN person p ON p.license_id = d.id
 GROUP BY license_id
 ORDER BY Licencias DESC
