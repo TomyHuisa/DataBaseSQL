a999a1e6416ab83d2839e33042a5b504a03202ca
Tomy Huisa <huisatomy2007@gmail.com>
2024-05-14 16:50:25 -0300

    Act6_Terminado

--- EvaluacionSQL/Act6.sql
+++ EvaluacionSQL/Act6.sql
@@ -1,5 +1,5 @@
 -- Act6 --
 
-SELECT count(membership_status) AS Category FROM get_fit_now_member 
+SELECT count(membership_status) AS Counts, membership_status AS Category FROM get_fit_now_member 
 GROUP BY membership_status
-ORDER BY Category DESC;
+ORDER BY Counts DESC;
