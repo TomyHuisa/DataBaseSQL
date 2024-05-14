48f15e687329e182029efdeb88707fb796bbfdd4
Tomy Huisa <huisatomy2007@gmail.com>
2024-05-14 16:07:55 -0300

    Act2_Terminado

--- EvaluacionSQL/Act2.sql
+++ EvaluacionSQL/Act2.sql
@@ -1,2 +1,7 @@
 -- Act2 --
 
+SELECT p.name, d.age, d.gender, d.plate_number, d.car_make, d.car_model 
+FROM drivers_license d 
+JOIN person p ON p.license_id = d.id
+WHERE d.gender LIKE 'Male' AND d.age > 70
+ORDER BY d.age ASC;
