5998018feede6e1d9ad9c8f64eebc3516578e974
Tomy Huisa <huisatomy2007@gmail.com>
2024-05-14 16:48:55 -0300

    Act6_Terminado

--- EvaluacionSQL/Act6.sql
+++ EvaluacionSQL/Act6.sql
@@ -1,23 +1,5 @@
 -- Act6 --
 
-SELECT count(g.membership_status) AS CategoriaGold FROM get_fit_now_member g 
-JOIN person p ON p.name = g.name
-WHERE g.membership_status LIKE 'gold'
-ORDER BY g.membership_status ASC;
-
---| 68 Miembros |--
--------------|
-
-SELECT count(g.membership_status) AS CategoriaSilver FROM get_fit_now_member g 
-JOIN person p ON p.name = g.name
-WHERE g.membership_status LIKE 'silver'
-
---| 63 Miembros |--
--------------|
-
-SELECT count(g.membership_status) AS CategoriaRegular FROM get_fit_now_member g 
-JOIN person p ON p.name = g.name
-WHERE g.membership_status LIKE 'regular'
-
---| 53 Miembros |--
--------------|
+SELECT count(membership_status) AS Category FROM get_fit_now_member 
+GROUP BY membership_status
+ORDER BY Category ASC;
