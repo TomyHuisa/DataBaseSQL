0825be87cc0f3b9b17ab0bcd67b76cc1355ef2ed
Tomy Huisa <huisatomy2007@gmail.com>
2024-05-14 16:30:33 -0300

    Act5_Terminado

--- EvaluacionSQL/Act5.sql
+++ EvaluacionSQL/Act5.sql
@@ -1,2 +1,7 @@
 -- Act5 --
 
+SELECT d.gender AS Genero, AVG(d.age) AS Edad_Promedio, MAX(d.age) AS Edad_Maxima, MIN(d.age) AS Edad_Minima
+FROM drivers_license d 
+JOIN person p ON p.license_id = d.id
+WHERE d.gender LIKE 'Female'
+GROUP BY d.gender;
