f71055aebef9e45f410109748aeef2d261882b2c
Tomy Huisa <Huisatomy2007@gmail.com>
2024-05-21 17:03:45 -0300

    Act7_TerminasoReciente

--- EvaluacionSQL/Act7.sql
+++ EvaluacionSQL/Act7.sql
@@ -1,8 +1,8 @@
--- Act7 --
+-- Act7 //Recien Terminado\\ --
 
 SELECT p.name, g.membership_status, p.ssn, gc.check_in_date FROM person p
 JOIN get_fit_now_member g ON p.id = g.person_id
 JOIN get_fit_now_check_in gc ON gc.membership_id = g.id
 WHERE gc.check_in_date LIKE '2018%'
 GROUP BY p.id
-ORDER BY g.membership_status
+ORDER BY g.membership_status ASC, p.ssn ASC
--- EvaluacionSQL/Act8.sql
+++ EvaluacionSQL/Act8.sql
@@ -5,3 +5,14 @@ JOIN person p ON d.id = p.license_id
 JOIN income c ON p.ssn = c.ssn
 WHERE c.annual_income < 35000
 ORDER BY annual_income ASC;
+
+
+-- Act8 //Modificado// --
+
+SELECT d.car_make, count(d.car_make), d.car_model, c.annual_income FROM drivers_license d
+JOIN person p ON d.id = p.license_id
+JOIN income c ON p.ssn = c.ssn
+WHERE c.annual_income < 35000
+GROUP by car_make
+HAVING count(car_make) > 4
+ORDER BY count(car_model) DESC;
